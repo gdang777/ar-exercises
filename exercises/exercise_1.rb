dd04be3a84e6dd1d59f2ac93aa170be2c2b3ce57
Gaurav <netgary7@gmail.com>
2018-11-06 21:35:16 +0000

    Completed exercise 5

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,7 @@ puts "----------"
 class Store < ActiveRecord::Base
 end
 
-burnaby = Store.create name: 'Burnaby',annual_revenue: 3000000, mens_apparel: true, womens_apparel: true
-richmond = Store.create name: 'Richmond',annual_revenue: 126000, mens_apparel: false, womens_apparel: true
+burnaby = Store.create name: 'Burnaby',annual_revenue: 300000, mens_apparel: true, womens_apparel: true
+richmond = Store.create name: 'Richmond',annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
 gastown = Store.create name: 'Gastown',annual_revenue: 198000, mens_apparel: true, womens_apparel: false
 puts Store.count
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum :annual_revenue
+@average_revenue = @total_revenue/Store.count
+
+puts "The total revenue of the entire company is #{@total_revenue}"
+puts "The average revenue of all the stores are #{@average_revenue}"
+
+@over_1M = Store.where('annual_revenue > 1000000')
+puts "Number of stores generating over 1M revenue is  #{@over_1M.count}"
